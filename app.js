c6ea893a344c17b931a6bbf16762a336628a7faa
ansar sattu <ansarthelingu95@gmail.com>
2023-02-14 14:09:46 +0800

    mencari nama berdasarkan nim

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const db = require("./connection");
-const respon = require("./responses");
+const express = require("express")
+const bodyParser = require("body-parser")
+const db = require("./connection")
+const respon = require("./responses")
 
-const app = express();
-const port = 5001;
+const app = express()
+const port = 5001
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 //ambil semua data product
 app.get("/", (req, res) => {
-  const sql = "SELECT * FROM product";
-
+  const sql = "SELECT * FROM product"
   db.query(sql, (err, result) => {
     // hasil
-    respon(200, result, "get all product", res);
-  });
-});
+    respon(200, result, "get all product", res)
+  })
+})
 
-app.get("/find", (err, res) => {
-  const sql = `SELECT product_name FROM product WHERE nim `;
-
+app.get("/find", (req, res) => {
+  const sql = `SELECT product_name FROM product WHERE nim = ${req.query.nim}`
+  // console.log("nim:", req.query.nim)
   db.query(sql, (err, result) => {
-    respon(200, result, "cari nama berdasarkan nim", res);
-  });
-});
+    // hasil
+    respon(200, result, "get name", res)
+  })
+})
 
 app.post("/login", (req, res) => {
-  console.log({ requestLogin: req.body });
-  res.send("login berhasil!");
-});
+  console.log({ requestLogin: req.body })
+  res.send("login berhasil!")
+})
 
 app.put("/user", (req, res) => {
-  console.log({ update: req.body });
-  res.send("update berhasil");
-});
+  console.log({ update: req.body })
+  res.send("update berhasil")
+})
 
 app.delete("/user", (req, res) => {
-  res.send("DELETE request at /user");
-});
+  res.send("DELETE request at /user")
+})
 
 //server
 app.listen(port, () => {
-  console.log(`localhost ${port}`);
-});
+  console.log(`localhost ${port}`)
+})
